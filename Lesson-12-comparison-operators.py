cc92ccbeb7e2b54d2be59919d955a974a5198091
shanto das <114013679+Shanto-123@users.noreply.github.com>
2024-09-25 03:10:37 +0600

    Add files via upload

---
+++ Lesson-12-comparison-operators.py
@@ -0,0 +1,9 @@
+price = 7
+if price == 10:
+    print('Buy This pen')
+    if price != 10:
+        print("It is not 10")
+        if price > 10:
+            print('It is expensive')
+            if price <= 10 :
+                print('It is cheap')
